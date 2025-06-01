8023d0bf38e88f6e4b4b7681febf665dca62dbf3
k4i-j <kaijief0@gmail.com>
2025-06-01 11:34:45 +0800

    script.js

--- script.js
+++ script.js
@@ -32,3 +32,31 @@ setInterval(() => {
   if (index >= track.children.length) index = 0;
   track.style.transform = `translateX(-${index * 100}%)`;
 }, 3000);
+
+const track = document.getElementById("carouselTrack");
+const slides = document.querySelectorAll(".carousel-slide");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+let currentSlide = 0;
+
+function updateCarousel() {
+  track.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+prevBtn.addEventListener("click", () => {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  updateCarousel();
+});
+
+nextBtn.addEventListener("click", () => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  updateCarousel();
+});
+
+// Auto scroll every 5 seconds
+setInterval(() => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  updateCarousel();
+}, 5000);
+
